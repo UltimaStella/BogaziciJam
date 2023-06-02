ab9cf1436ce77df87b61b3d27fd9e13a7a6a638a
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-06-02 22:37:15 +0300

    UI iskeleti eklendi 0.1.0

---
+++ Assets/Gulsoy/Scripts/UI/Manager/MenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        public GameObject[] panels;
+
+        [HideInInspector]
+        public static void OpenPanel(GameObject panel) { panel.SetActive(true); }
+        public static void ClosePanel(GameObject panel) { panel.SetActive(false); }
+        public static void PopUpAds(GameObject panel) { panel.SetActive(false); }
+        public void OpenSelectedPanel(GameObject selectedPanel)
+        {
+            foreach (GameObject panel in this.panels) ClosePanel(panel);
+
+            OpenPanel(selectedPanel);
+        }
+
+        //public static void LoadScene(string sceneName)
+        //{
+        //    SceneManager.LoadScene(sceneName);
+        //}
+        public  void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        public void ClickExit()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Gulsoy/Scripts/UI/Menus/InGameMenu/InGameMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Assets.Scripts.UI.Manager;
+
+namespace Assets.Scripts.UI.InGameMenu
+{
+
+    public class InGameMenu : MenuManager
+    {
+        public enum buttons { gas, breaks, turbo };
+
+        [SerializeField]
+        GameObject car;
+
+
+
+        bool[] buttonsValue;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            buttonsValue = new bool[3];
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+
+
+
+        }
+
+        public void PointerDown(int index)
+        {
+            buttonsValue[index] = true;
+        }
+
+        public void PointerUp(int index)
+        {
+            buttonsValue[index] = false;
+        }
+        public void PauseGame()
+        {
+            Time.timeScale = 0f;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
