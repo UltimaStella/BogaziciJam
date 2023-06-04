6bd5a5750fa7c8581bfe7999a12e72da38872c5b
Mustafa Toprak <MToprak.L203@gmail.com>
2023-06-04 12:47:16 +0300

    camera move area changed

--- Assets/Toprak/Script/CameraPosChangeArea.cs
+++ Assets/Toprak/Script/CameraPosChangeArea.cs
@@ -7,8 +7,15 @@ public class CameraPosChangeArea : MonoBehaviour
     [SerializeField] Transform CameraHolder;
     [SerializeField] Transform startPoint;
     [SerializeField] Transform endPoint;
+    [SerializeField] Vector3 newOffset;
 
     public bool isPlayerInArea;
+
+    Vector3 initOffset;
+    Vector3 lastOffset;
+
+    bool hasInitOffset = false;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +23,11 @@ public class CameraPosChangeArea : MonoBehaviour
         {
             isPlayerInArea = true;
             CameraHolder.GetComponent<CameraMovement>().enabled = false;
+            if (!hasInitOffset) 
+            { 
+                initOffset = CameraHolder.GetComponent<CameraMovement>().Offset; 
+                hasInitOffset = true;
+            }
         }
     }
 
@@ -26,6 +38,37 @@ public class CameraPosChangeArea : MonoBehaviour
             isPlayerInArea = false;
             CameraHolder.GetComponent<CameraMovement>().UpdateOffset();
             CameraHolder.GetComponent<CameraMovement>().enabled = true;
+            lastOffset = CameraHolder.GetComponent<CameraMovement>().Offset;
+            if (Vector3.Distance(startPoint.position, Player.Instance.transform.position)
+                <
+                Vector3.Distance(endPoint.position, Player.Instance.transform.position))
+            {
+                StartCoroutine(ToInitOffset());
+            }
+            else
+            {
+                StartCoroutine(ToNewOffset());
+            }
+        }
+    }
+
+    IEnumerator ToNewOffset()
+    {
+        while (Vector3.Distance(newOffset, lastOffset) > 0.01)
+        {
+            lastOffset = Vector3.Lerp(lastOffset, newOffset, Time.deltaTime);
+            CameraHolder.GetComponent<CameraMovement>().Offset = lastOffset;
+            yield return null;
+        }
+    }
+
+    IEnumerator ToInitOffset()
+    {
+        while (Vector3.Distance(initOffset, lastOffset) > 0.01)
+        {
+            lastOffset = Vector3.Lerp(lastOffset, initOffset, Time.deltaTime);
+            CameraHolder.GetComponent<CameraMovement>().Offset = lastOffset;
+            yield return null;
         }
     }
 }
